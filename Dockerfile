8beb9fafa95407d6c1bd200d62737e24e45e249c
bcapuano <bcapuano@asu.edu>
2022-08-06 11:29:26 -0400

    Made requested changes

--- Dockerfile
+++ Dockerfile
@@ -3,26 +3,18 @@ FROM --platform=linux/amd64 ubuntu:20.04 as builder
 
 ## Install build dependencies.
 RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y clang make git libc6-dbg netcat
+    DEBIAN_FRONTEND=noninteractive apt-get install -y clang make
 
 ## Add source code to the build stage.
-ADD https://api.github.com/repos/capuanob/znet/git/refs/heads/mayhem version.json
-RUN git clone -b mayhem https://github.com/capuanob/znet.git
+ADD . /znet
 WORKDIR /znet
 
 ## Build fuzzer
-RUN clang -O0 -g -fsanitize=fuzzer fuzz/fuzz.c -o znet-fuzz
-
-## Package dependencies
-RUN mkdir /deps
-RUN cp `ldd znet-fuzz | grep so | sed -e '/^[^\t]/ d' | sed -e 's/\t//' | sed -e 's/.*=..//' | sed -e 's/ (0.*)//' | sort | uniq` /deps 2>/dev/null || :
+RUN clang -O0 -g -fsanitize=fuzzer fuzz/fuzz_znet_server.c -o znet-fuzz
 
 # Package Stage
 FROM --platform=linux/amd64 ubuntu:20.04
-RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y libc6-dbg netcat
 COPY --from=builder /znet/znet-fuzz /znet-fuzz
-COPY --from=builder /deps /usr/lib
 
-CMD /znet-fuzz -close_fd_mask=2
+CMD /znet-fuzz
 
--- tests/bench_server.c
+++ tests/bench_server.c
@@ -9,10 +9,9 @@
 /* server */
 
 zn_State *S;
-zn_Accept *a;
 zn_BufferPool pool;
 
-#define INTERVAL 100
+#define INTERVAL 5000
 int send_count = 0;
 int recv_count = 0;
 int connect_count = 0;
@@ -40,8 +39,6 @@ static void on_recv(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
     zn_recv(tcp,
             zn_recvbuff(&node->recv),
             zn_recvsize(&node->recv), on_recv, ud);
-    zn_closetcp(tcp);
-    zn_deltcp(tcp);
 }
 
 static size_t on_header(void *ud, const char *buff, size_t len) {
@@ -118,18 +115,16 @@ static void on_interrupted(int signum) {
 }
 
 static void register_interrupted(void) {
-   struct sigaction act;
+   struct sigaction act; 
    act.sa_flags = SA_RESETHAND;
    act.sa_handler = on_interrupted;
-   /* Signals blocked during the execution of the handler. */
-   sigemptyset(&act.sa_mask);
-   sigaddset(&act.sa_mask, SIGINT);
    sigaction(SIGINT, &act, NULL);
 }
 #endif
 
 int main(int argc, char **argv) {
     unsigned port = 12345;
+    zn_Accept *accept;
     zn_Timer *timer;
     if (argc == 2) {
         unsigned p = atoi(argv[1]);
@@ -142,11 +137,11 @@ int main(int argc, char **argv) {
     if (S == NULL)
         return 2;
 
-    a = zn_newaccept(S, 0);
-    if (a == NULL)
+    accept = zn_newaccept(S);
+    if (accept == NULL)
         return 2;
-    zn_listen(a, "0.0.0.0", port);
-    zn_accept(a, on_accept, NULL);
+    zn_listen(accept, "0.0.0.0", port);
+    zn_accept(accept, on_accept, NULL);
     printf("listening at: %u\n", port);
 
     timer = zn_newtimer(S, on_timer, NULL);
